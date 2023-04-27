f8358af0b35a7d169ff32d87ae499b0972d7ed68
felipera8732 <felipe@pruvo.com>
2023-04-27 10:34:38 -0300

    Added some SEO to pages using next

--- src/pages/schedule/[username]/index.page.tsx
+++ src/pages/schedule/[username]/index.page.tsx
@@ -1,6 +1,7 @@
 import { Avatar, Heading, Text } from '@felipe_ignite_ui/react'
 import { GetStaticPaths, GetStaticProps } from 'next'
 import { prisma } from '../../../lib/prisma'
+import { NextSeo } from 'next-seo'
 import { ScheduleForm } from './ScheduleForm'
 import { Container, UserHeader } from './styles'
 
@@ -14,15 +15,19 @@ interface ScheduleProps {
 
 export default function Schedule({ user }: ScheduleProps) {
   return (
-    <Container>
-      <UserHeader>
-        <Avatar src={user.avatarUrl} />
-        <Heading>{user.name}</Heading>
-        <Text>{user.bio}</Text>
-      </UserHeader>
+    <>
+      <NextSeo title={`Agendar com ${user.name} | Ignite Call`} />
 
-      <ScheduleForm />
-    </Container>
+      <Container>
+        <UserHeader>
+          <Avatar src={user.avatarUrl} />
+          <Heading>{user.name}</Heading>
+          <Text>{user.bio}</Text>
+        </UserHeader>
+
+        <ScheduleForm />
+      </Container>
+    </>
   )
 }
 
